cb3d8987ee1dfc0c2bf895ad064fc4c166885c8a
kawahara <23jy0211@jec.ac.jp>
2024-10-23 12:19:09 +0900

    変数a,b,c,x,y,zを追加

---
+++ Helloworld.java
@@ -0,0 +1,15 @@
+public class Hello
+    public static void main (String[]args{
+        System.out.println("Helloworld");
+        System.out.println("Helloworld");
+        System.out.println("Helloworld");
+    }
+        int a,b,c;
+        double x,y,z
+
+
+
+ターミナル
+git config --global user.name'kawahara'
+git config --global user.email'23jy0211@jec.ac.jp'
+
