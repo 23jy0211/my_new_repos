3f6b30cf03fb61d6346219f72726dbe8f2b44aaf
kawahara <23jy0211@jec.ac.jp>
2024-10-23 12:26:56 +0900

    変数を追加した

--- Helloworld.java
+++ Helloworld.java
@@ -1,15 +1,16 @@
-public class Hello
-    public static void main (String[]args{
+public class Hello{
+    public static void main (String[]args){
         System.out.println("Helloworld");
         System.out.println("Helloworld");
         System.out.println("Helloworld");
-    }
+    
         int a,b,c;
         double x,y,z
-
+    }
+}
 
 
 ターミナル
-git config --global user.name'kawahara'
-git config --global user.email'23jy0211@jec.ac.jp'
+git config --global user.name 'kawahara'
+git config --global user.email '23jy0211@jec.ac.jp'
 
